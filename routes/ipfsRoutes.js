36c2d44b0b9fd0aae1cce9d81607c197fdcd14b5
Alister Porteous <alisterporteous@hotmail.com>
2023-05-31 13:11:29 -0400

    added JWT to ipfsRoutes.js

--- routes/ipfsRoutes.js
+++ routes/ipfsRoutes.js
@@ -4,6 +4,7 @@ const express = require("express");
 const router = express.Router();
 const fetch = require("node-fetch");
 const FormData = require("form-data");
+const { Readable } = require("stream"); // Import Readable from 'stream'
 
 router.post("/upload-to-ipfs", async (req, res) => {
   const { imageUrl } = req.body;
@@ -11,20 +12,31 @@ router.post("/upload-to-ipfs", async (req, res) => {
   const response = await fetch(imageUrl);
   const data = await response.buffer();
 
+  // Convert the buffer to a readable stream
+  const readableStream = new Readable();
+  readableStream.push(data);
+  readableStream.push(null);
+
   const formData = new FormData();
-  formData.append("file", data);
+  formData.append("file", readableStream, {
+    filename: "image.jpg",
+    contentType: "image/jpeg",
+    knownLength: data.length,
+  }); // Adjust the filename and contentType as necessary
 
   const result = await fetch("https://api.pinata.cloud/pinning/pinFileToIPFS", {
     method: "POST",
     headers: {
-      pinata_api_key: process.env.PINATA_API_KEY,
-      pinata_secret_api_key: process.env.PINATA_SECRET_API_KEY,
+      Authorization: "Bearer " + process.env.PINATA_JWT,
     },
     body: formData,
   });
 
   if (!result.ok) {
-    res.status(500).json({ error: "Error uploading file to Pinata" });
+    const errorDetails = await result.json(); // To get more details about the error
+    res
+      .status(500)
+      .json({ error: "Error uploading file to Pinata", details: errorDetails });
     return;
   }
 
